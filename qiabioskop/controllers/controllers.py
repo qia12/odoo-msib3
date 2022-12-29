f3c455c5e7383d00ba6c6dbc07bf100d6f456f42
qia <nibras.alfaruqiyah@hashmicro.co.id>
2022-12-29 10:56:54 +0700

    first commit

--- qiabioskop/controllers/controllers.py
+++ qiabioskop/controllers/controllers.py
@@ -39,16 +39,80 @@ class Qiabioskop(http.Controller):
             return obj.isoformat() if hasattr(obj, 'isoformat') else obj
         return json.dumps(sup, default=date_handler)
     
-    # @http.route('/qiabioskop/getgenre', auth='public', method=['GET'])
-    # def getGenre(self, **kw):
-    #     genre = request.env['qiabioskop.genre'].search([])
-    #     gen = []
-    #     for gg in genre:
-    #         gen.append({
-    #             'nama_genre' : gg.name,
-    #             'kode_genre' : gg.kd_genre,
-    #             'daftar_film' : gg.film_ids[0].genre_no,
-    #             'jumlah_film' : gg.jml_film,
-    #             'pilihan' : gg.pilihan
-    #         })
-    #     return json.dumps(gen)
+    @http.route('/qiabioskop/getjadwal', auth='public', method=['GET'])
+    def getJadwal(self, **kw):
+        jadwal = request.env['qiabioskop.jadwal'].search([])
+        jad = []
+        for jj in jadwal:
+            jad.append({
+                'tanggal_tayang' : jj.name,
+                'jam_mulai' : jj.jam_mulai,
+                'jam_berakhir' : jj.jam_berakhir,
+                'daftar_pelanggan' : jj.pelanggan_id[0].id,
+                'daftar_film' : jj.film_id[0].name
+            })
+        def date_handler(obj):
+            return obj.isoformat() if hasattr(obj, 'isoformat') else obj
+        return json.dumps(jad, default=date_handler)
+    
+    @http.route('/qiabioskop/getgenre', auth='public', method=['GET'])
+    def getGenre(self, **kw):
+        genre = request.env['qiabioskop.genre'].search([])
+        gen = []
+        for gg in genre:
+            gen.append({
+                'nama_genre' : gg.name,
+                'kode_genre' : gg.kd_genre,
+                'daftar_film' : gg.film_ids[0].name,
+                'jumlah_film' : gg.film_ids[0].name,
+                'pilihan' : gg.film_ids[0].name
+            })
+        return json.dumps(gen)
+
+    @http.route('/qiabioskop/getkursi', auth='public', method=['GET'])
+    def getKursi(self, **kw):
+        kursi = request.env['qiabioskop.kursi'].search([])
+        kur = []
+        for kk in kursi:
+            kur.append({
+                'nomor_kursi' : kk.name,
+                'kode_kursi' : kk.kd_kursi,
+                'tiket' : kk.tiket_ids[0].name,
+                'daftar_studio' : kk.studio_id[0].name
+            })
+        return json.dumps(kur)
+
+    @http.route('/qiabioskop/getstudio', auth='public', method=['GET'])
+    def getStudio(self, **kw):
+        studio = request.env['qiabioskop.studio'].search([])
+        stu = []
+        for ii in studio:
+            stu.append({
+                'nama_studio' : ii.name,
+                'kode_studio' : ii.kd_studio,
+                'daftar_kursi' : ii.kursi_ids[0].name,
+                'jumlah_kursi' : ii.kursi_ids[0].name,
+                'pilihan' : ii.pilihan,
+                'daftar_film' : ii.film_studio_id[0].name
+            })
+        return json.dumps(stu)
+
+    @http.route('/qiabioskop/getpenjualan', auth='public', method=['GET'])
+    def getStudio(self, **kw):
+        penjualan = request.env['qiabioskop.penjualan'].search([])
+        pen = []
+        for nn in penjualan:
+            pen.append({
+                'no_nota' : nn.name,
+                'nama_pembeli' : nn.id,
+                'ID_member' : nn.id_member,
+                'tanggal_penjualan' : nn.tgl_penjualan,
+                'ttl_bayar' : nn.total_bayar,
+                'detail_penjualan' : nn.detailpenjualan_ids[0].name,
+                'status' : nn.state,
+            })
+        return json.dumps(pen)
+
+    
+
+        
--- qiabioskop/models/Jadwal.py
+++ qiabioskop/models/Jadwal.py
@@ -5,9 +5,35 @@ class Jadwal(models.Model):
     _name = 'qiabioskop.jadwal'
     _description = 'Jadwal'
        
-    name = fields.Date(string='Tanggal Tayang')
-    jam_mulai = fields.Datetime(string='Jam Mulai')
-    jam_berakhir = fields.Datetime(string='Jam Berakhir')
+    name = fields.Selection([
+        ('14/09/2022', '14/09/2022'), 
+        ('15/09/2022', '15/09/2022'), 
+        ('16/09/2022', '17/09/2022'),
+        ], string='Tanggal Tayang')
+
+    jam_mulai = fields.Selection([
+        ('10:00:00', '10:00:00'), 
+        ('13:00:00', '13:00:00'), 
+        ('15:00:00', '15:00:00'),
+        ('18:15:00', '18:15:00'),
+        ('20:15:00', '20:15:00'),
+        ], string='Jam Mulai')
+
+    jam_berakhir = fields.Char(onchange='_compute_jam_mulai', string='Jam Berakhir')
+
+    @api.onchange('jam_mulai')
+    def _onchange_jam_berakhir(self):
+        if (self.jam_mulai == '10:00:00'):
+            self.jam_berakhir = '12:40:00'
+        elif (self.jam_mulai == '13:00:00'):
+            self.jam_berakhir = '13:40:00'
+        elif (self.jam_mulai == '15:00:00'):
+            self.jam_berakhir = '17:40:00'
+        elif (self.jam_mulai == '18:15:00'):
+            self.jam_berakhir = '20:40:00'
+        elif (self.jam_mulai == '20:15:00'):
+            self.jam_berakhir = '22:45:00'
+        
     pelanggan_id = fields.Many2one(comodel_name='res.partner', 
                                    string='Daftar Pelanggan',
                                    ondelete='cascade')
